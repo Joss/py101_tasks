d28df187a9825559e44a19fcf55f9b7c05bf016c
Joss <Joss@users.noreply.github.com>
2020-10-30 11:02:10 +0300

    Done

--- guess_num.py
+++ guess_num.py
@@ -14,5 +14,25 @@
 Тебе может понадобится модуль random, цикл while и ветвления
 """
 
+import random
+
+def task_3():
+    UPPER_LIMIT = 1000000
+
+    random_value = random.randint(0, UPPER_LIMIT)
+    input_value = input(f"Enter your guess in 0-{UPPER_LIMIT}: ")
+    while input_value not in ("", "exit"):
+        if not input_value.isdigit() or (integer_value := int(input_value)) > UPPER_LIMIT:
+            print("Wrong enter")
+        else:
+            if (integer_value == random_value):
+                print(f"Your are right!")
+                break
+
+            hint = "greater" if integer_value > random_value else "less"
+            print(f"Your guess is {hint} then the value")
+
+        input_value = input(f"Enter your guess in 0-{UPPER_LIMIT}: ")
+
 if __name__ == '__main__':
-    pass
+    task_3()
