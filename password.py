97ffb6209520982ad036340a66ec3db3a68f0f3f
Joss <Joss@users.noreply.github.com>
2020-10-30 11:20:31 +0300

    Done

--- password.py
+++ password.py
@@ -5,7 +5,7 @@
 Сложным считается пароль, состоящий как минимум из 8-ми символов,
 включая цифры и алфавитные символы
 """
-import re
+import re 
 
 if __name__ == '__main__':
     print(f"The password is {'stong' if re.fullmatch(r'[A-Za-z0-9]{8,}', input('Enter new password: ')) else 'weak'}")
